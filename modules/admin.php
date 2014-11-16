abb8eb56745bd9ba9cb243273edc5fc912796638
SolidX <gaf256@nyu.edu>
2014-11-16 17:37:49 -0500

    Converts old modules to use new module format.

--- modules/admin.php
+++ modules/admin.php
@@ -1,55 +1,88 @@
 <?php
-/**************************************************
- *
- * IRCsmasher - based on IRCmasher by Ralf Oechsner and Volker Latainski
- *
- * @Project: IRCsmasher
- * @License: GNU General Public License v2
- *
- **************************************************/
+    /**************************************************
+     *
+     * IRCsmasher - based on IRCmasher by Ralf Oechsner and Volker Latainski
+     *
+     * @Project: IRCsmasher
+     * @License: GNU General Public License v2
+     *
+     **************************************************/
 
-function admin ($output, $com1, $com2, $com3, $com4, $name, $begin, $chan, $command, $message) {
+    require_once("./BaseBotModule.php");
 
-  global $ircsocket, $server, $port, $nick, $channel, $real_name, $botpw, $ident, $nickserv; 
-  
-  //split message into single words
-  $arguments = explode(" ",$message);
+    class Admin extends BaseBotModule
+    {		
+        public function __construct($socket, $ircserver, $portNumber, $myNick, $channels, $realName, $botPword)
+        {
+            parent::__construct($socket, $ircserver, $portNumber, $myNick, $channels, $realName, $botPword);
+            $this->module_version = "1.0";
+        }
+        
+        function runModule ($output, $com1, $com2, $com3, $com4, $name, $begin, $chan, $command, $message)
+        {
+            //split message into single words
+            $arguments = explode(" ", $message);
 
-  if ($arguments[0] == "giveop" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("MODE $arguments[2] +o $arguments[3]");
-  }
+            if ($arguments[0] == "giveop" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("MODE $arguments[2] +o $arguments[3]");
+            }
 
-  if ($arguments[0] == "takeop" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("MODE $arguments[2] -o $arguments[3]");
-  }
+            if ($arguments[0] == "takeop" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("MODE $arguments[2] -o $arguments[3]");
+            }
 
-  if ($arguments[0] == "voice" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("MODE $arguments[2] +v $arguments[3]");
-  }
+            if ($arguments[0] == "voice" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("MODE $arguments[2] +v $arguments[3]");
+            }
 
-  if ($arguments[0] == "dvoice" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("MODE $arguments[2] -v $arguments[3]");
-  }
-  
-  if ($arguments[0] == "kick" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("KICK $arguments[2] $arguments[3]");
-  }
-  
-  if ($arguments[0] == "identify" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    write_socket("NICK $nick");
-    write_socket("PRIVMSG $nickserv IDENTIFY $ident");
-  }
+            if ($arguments[0] == "dvoice" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("MODE $arguments[2] -v $arguments[3]");
+            }
 
-  //let the bot talk for you :-)
-  if ($arguments[0] == "say" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-      $message = str_replace("say","",$message);
-      $message = str_replace("$botpw","",$message);
-#     $message = str_replace("$arguments[2]","",$message);
-      priv_msg($channel, ltrim($message));   
-  }
-  
-#  if ($arguments[0] == "restart" && $arguments[1] == $botpw && $command == "PRIVMSG") {
-    #doesn't work yet
-#  }
-}
+            if ($arguments[0] == "kick" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("KICK $arguments[2] $arguments[3]");
+            }
+
+            if ($arguments[0] == "identify" && $arguments[1] == $this->botpw && $command == "PRIVMSG") {
+                write_socket("NICK $nick");
+                write_socket("PRIVMSG $nickserv IDENTIFY $ident");
+            }
+            
+            //let the bot talk for you :-)
+            if (preg_match("/say [A-Za-z0-9_\-#]+ ".$this->botpw."/i", $output))
+            {
+                $message = substr($output, stripos($output, ":", 1) + 1);
+                $startPt = stripos($message, "say");
+                $message = substr($message, 0, $startPt) . substr($message, $startPt + 3);
+                $message = str_ireplace("say", "", $message, $limit);
+                $message = str_ireplace($this->botpw, "", $message);
+                $message = str_ireplace($arguments[1], "", $message);
+                $message = trim($message);
+
+                echo "Say \"$message\" to ".$arguments[1];
+
+                if ($arguments[1] == "all")
+                {
+                    foreach($this->channel as $target)
+                    {
+                        priv_msg($target, $message);
+                    }
+                }
+                else
+                    priv_msg($arguments[1], $message);
+                
+                return;
+            }
+            
+            #  if ($arguments[0] == "restart" && $arguments[1] == $botpw && $command == "PRIVMSG") {
+            #doesn't work yet
+            #  }
+        }
+        
+        public function getTriggers($user)
+        {
+            //TODO: Implement getTriggers
+            return;
+        }
+    }
 ?>
--- modules/peak.php
+++ modules/peak.php
@@ -1,83 +1,102 @@
 <?php
+    /**************************************************
+     *
+     * IRCsmasher - based on IRCmasher by Ralf Oechsner and Volker Latainski
+     *
+     * @Project: IRCsmasher
+     * @License: GNU General Public License v2
+     *
+     **************************************************/
 
-/**************************************************
- *
- * IRCsmasher - based on IRCmasher by Ralf Oechsner and Volker Latainski
- *
- * @Project: IRCsmasher
- * @License: GNU General Public License v2
- *
- **************************************************/
+    require_once("./BaseBotModule.php");
 
-function peak_read($peak_datapath) {
-    $peak_handle = fopen($peak_datapath, "r");
-    $peak_data = fread($peak_handle, filesize($peak_datapath));
-    $peak_data_split = explode("|", $peak_data);
-    fclose($peak_handle);
-    return $peak_data_split;
-}
+    class Peak extends BaseBotModule
+    {
+        public function __construct($socket, $ircserver, $portNumber, $myNick, $channels, $realName, $botPword)
+        {
+            parent::__construct($socket, $ircserver, $portNumber, $myNick, $channels, $realName, $botPword);
+            $this->module_version = "1.0";
+        }
+        
+        private function peak_read($peak_datapath)
+        {
+            $peak_handle = fopen($peak_datapath, "r");
+            $peak_data = fread($peak_handle, filesize($peak_datapath));
+            $peak_data_split = explode("|",$peak_data);
+            fclose($peak_handle);
+            return $peak_data_split;
+        }
+        
+        private function peak_write($count, $timestamp, $peak_datapath)
+        {
+            $new_peak_data = $count . "|" . $timestamp;
+            $new_peak_handle = fopen($peak_datapath, "w");
+            fwrite($new_peak_handle, $new_peak_data);
+            fclose($new_peak_handle);
+        }
+        
+        public function runModule($output, $com1, $com2, $com3, $com4, $name, $begin, $chan, $command, $message)
+        {
+            $str_namelist = explode(" ", $com3);
+            $count = count($str_namelist) - 1;
+            $namelist_code = explode(" ", $com2);
+            $timestamp = time();
 
-function peak_write($count, $timestamp, $peak_datapath) {
-    $new_peak_data = $count . "|" . $timestamp;
-    $new_peak_handle = fopen($peak_datapath, "w");
-    fwrite($new_peak_handle, $new_peak_data);
-    fclose($new_peak_handle);
-}
+            if($namelist_code[1] == "353")
+            {
+                $peak_datafile = "peak_" . $namelist_code[4] . ".data";
+                $peak_datapath = "modules/data/" . $peak_datafile;
 
-function peak($output, $com1, $com2, $com3, $com4, $name, $begin, $chan, $command, $message) {
+                if(filesize($peak_datapath) > "1")
+                {
+                    $new_peak_data_split = $this->peak_read($peak_datapath);
 
-    global $ircsocket, $server, $port, $nick, $channel, $chan, $real_name, $botpw, $incoming;
-
-    $str_namelist = explode(" ", $com3);
-    $count = count($str_namelist) - 1;
-    $namelist_code = explode(" ", $com2);
-    $timestamp = time();
-
-    if ($namelist_code[1] == "353") {
-        $peak_datafile = "peak_" . $namelist_code[4] . ".data";
-        $peak_datapath = "modules/data/" . $peak_datafile;
-
-        if (filesize($peak_datapath) > "1") {
-            $new_peak_data_split = peak_read($peak_datapath);
-
-            if ($new_peak_data_split[0] < $count) {
-                peak_write($count, $timestamp, $peak_datapath);
+                    if($new_peak_data_split[0] < $count)
+                        $this->peak_write($count, $timestamp, $peak_datapath);
+                }
+                else
+                    $this->peak_write($count, $timestamp, $peak_datapath);
             }
-        } else {
-            peak_write($count, $timestamp, $peak_datapath);
+
+            //on join => check for users
+            $join_signs = explode(" ", $com2);
+            if($join_signs[1] == "JOIN" && $name != $real_name)
+            {
+                write_socket("NAMES $com3");
+                $peak_datafile = "peak_" . rtrim($com3) . ".data";
+                $peak_datapath = "modules/data/" . $peak_datafile;
+
+                if(filesize($peak_datapath) > "1")
+                {
+                    $new_peak_data_split = $this->peak_read($peak_datapath);
+
+                    if($peak_data_split[0] < $count)
+                    {
+                        $this->peak_write($count, $timestamp, $peak_datapath);
+                    }
+                }
+                else
+                   $this->peak_write($count, $timestamp, $peak_datapath);
+            }
+
+            //!peak
+            if (preg_match("/!peak/i", $message))
+            {
+                $peak_datafile = "peak_" . $namelist_code[2] . ".data";
+                $peak_datapath = "modules/data/" . $peak_datafile;
+
+                $new_peak_data_split = $this->peak_read($peak_datapath);
+
+                $peak_timestamp = date("D. M j, Y g:i:s A T", $new_peak_data_split[1]);
+                $peak_msg = "The peak for $namelist_code[2] is $new_peak_data_split[0] users. It was reached on $peak_timestamp";
+                delay_priv_msg($namelist_code[2], "$peak_msg", "6");
+                return;
+            }
+        }
+        
+        public function getTriggers($user)
+        {
+            notice_msg(parent::parseName($user), "Get Channel Peak Users: !peak");
         }
     }
-
-    //on join => check for users
-    $join_signs = explode(" ", $com2);
-    if ($join_signs[1] == "JOIN" && $name != $real_name) {
-        write_socket("NAMES $com3");
-        $peak_datafile = "peak_" . rtrim($com3) . ".data";
-        $peak_datapath = "modules/data/" . $peak_datafile;
-
-        if (filesize($peak_datapath) > "1") {
-
-            $new_peak_data_split = peak_read($peak_datapath);
-
-            if ($peak_data_split[0] < $count) {
-                peak_write($count, $timestamp, $peak_datapath);
-            }
-        } else {
-            peak_write($count, $timestamp, $peak_datapath);
-        }
-    }
-
-    //!peak
-    if (preg_match("/!peak/i", $message)) {
-        $peak_datafile = "peak_" . $namelist_code[2] . ".data";
-        $peak_datapath = "modules/data/" . $peak_datafile;
-
-        $new_peak_data_split = peak_read($peak_datapath);
-
-        $peak_timestamp = date("D, j.M.Y G:i:s", $new_peak_data_split[1]);
-        $peak_msg = "The peak for $namelist_code[2] is $new_peak_data_split[0] users. It was set on $peak_timestamp";
-        delay_priv_msg($namelist_code[2], "$peak_msg", "6");
-    }
-}
-
 ?>
